1d7c82f12bad62fe81e65765f61cf6eac625e641
AL1 <alibinkashif007@gmail.com>
2025-05-17 22:55:25 +0500

    Update lambda funtion

--- lambda_funcs/app.py
+++ lambda_funcs/app.py
@@ -5,6 +5,6 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hello from Lambda on Windows!",
+        "body": "Hello from Lambda!",
         "success": True
     }
