04816ddb3a8eac7f08a6299f45627f381016fd6f
AL1 <alibinkashif007@gmail.com>
2025-05-17 18:36:20 +0500

    Update project structure

--- lambda_funcs/fetch_crypto_data_api/main.py
+++ lambda_funcs/fetch_crypto_data_api/main.py
@@ -5,5 +5,6 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hello from Lambda on Windows!"
+        "body": "Hello from Lambda on Windows!",
+        "success": True
     }
